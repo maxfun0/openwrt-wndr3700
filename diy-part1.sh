9f34d7f085878b3d3df33f3a497d160443f1d68b
maxfun0 <31366029+maxfun0@users.noreply.github.com>
2022-03-14 15:47:30 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,16 +11,16 @@
 #
 
 # DIY
-git clone https://github.com/xiaorouji/openwrt-passwall
-git clone https://github.com/kenzok8/small-package
+# git clone https://github.com/xiaorouji/openwrt-passwall
+# git clone https://github.com/kenzok8/small-package
 
 
 # Uncomment a feed source
-# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 # sed -i 's/^#\(.*passwall\)/\1/' feeds.conf.default
 # sed -i 's/^#\(.*small8\)/\1/' feeds.conf.default
 
 # Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+#echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
